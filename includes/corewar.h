82b05c08633e416d05834f40583fef1fce9fb5d5
pablo-a <pabloabril@hotmail.fr>
2016-06-06 13:39:48 +0200

    j'ai rajoute les op.h et op.c et regarde un peu le corewar.h

--- includes/corewar.h
+++ includes/corewar.h
@@ -1,10 +1,34 @@
-//
-// Created by mcotfas on 03/06/16.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   corewar.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabril <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/06/06 12:44:14 by pabril            #+#    #+#             */
+/*   Updated: 2016/06/06 13:39:12 by pabril           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef COREWAR_H
 # define COREWAR_H
 
+/* On va pouvoir utiliser ERRNO c'est de la balle :
+** if (malloc(...) == -1)
+**		ft_printf("erreur : %s\n", strerror(errno));
+*/
+#include <errno.h>
+#include <string.h>
 #include "../libft/includes/libftprintf.h"
+
+/* --------------------- STRUCTURE PRINCIPALE ---------------------------------
+** CYCLE_TO_DIE est une variable centrale du jeu, on en aura besoin.
+** les autres peuvent rester des define dans le op.h je pense
+*/
+
+typedef struct	s_env
+{
+	int		cycle_to_die;
+}				t_env;
 
 #endif
